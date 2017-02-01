d0b708cc6a19c0c56fc6ba49296fea13f137271e
emRae <emRae@users.noreply.github.com>
2017-01-31 20:45:58 -0700

    adding error handling

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 var http = require('http');
+// fs stands for file system
+var fs = require('fs');
+var EventEmitter = require('events').EventEmitter;
+
 var port = '8080';
+var logger = new EventEmitter();
+
+logger.on('error', function(err){
+	console.log('ERROR: ' + err);
+});
 
 http.createServer( function(request, response){
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.end("My first node app worked!");
+	fs.readFile('index.html', function(err,content) {
+		if(err){
+			response.writeHead(404, {'Content-Type': 'text/html'});
+			logger.emit('error', err);
+			response.end('failed');
+		} else {
+			response.writeHead(200, {'Content-Type': 'text/html'});
+			response.end(content);
+		}
+	});
 }).listen(port);
 
 console.log('server running on port:' + port);
